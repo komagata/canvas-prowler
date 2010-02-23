d2a215b37277677bfeb557426b03d82c7399a1dd
Masaki KOMAGATA <komagata@gmail.com>
2010-02-23 22:52:43 +0900

    fixed

--- js/game.js
+++ js/game.js
@@ -3,23 +3,34 @@ function Point(x, y) {
   this.y = y
 }
 
-function Stage(canvas) {
+function Game(canvas) {
   this.canvas = canvas
   this.context = canvas.getContext('2d')
   this.nodes = []
-  this.fps = 4
+  this.fps = 24
   this.click = new Point(0, 0)
+  this.player = null
   var self = this
   canvas.onclick = function(evt){ self.onclick(evt) }
 }
-Stage.prototype = {
+Game.prototype = {
   add: function(node) {
-    node.stage = this
+    node.game = this
     this.nodes.push(node)
   },
+  addPlayer: function(player) {
+    this.player = player
+    this.add(player)
+  },
   onenterframe: function() {
+    this.draw()
     for (var i = 0; i < this.nodes.length; i++)
       this.nodes[i].onenterframe()
+  },
+  draw: function() {
+    this.context.beginPath()
+    this.context.fillStyle = 'rgb(128, 128, 128)'
+    this.context.fillRect(0, 0, 256, 256)
   },
   start: function() {
     var self = this
@@ -28,13 +39,14 @@ Stage.prototype = {
     }, 1000 / self.fps)
   },
   onclick: function(evt) {
-    this.click.x = evt.offsetX
-    this.click.y = evt.offsetY
+    var x = evt.layerX || evt.offsetX
+    var y = evt.layerY || evt.offsetY
+    this.player.buildPath(new Point(x, y))
   }
 }
 
 function Sprite() {
-  this.stage = null
+  this.game = null
   this.x = 0
   this.y = 0
 }
@@ -52,24 +64,34 @@ function Hunter() {
     w: 'img/hunter_w0.png'
   }
   this.dir = 's'
-  this.mx = 2
-  this.my = 2
+  this.distance = 2
+  this.path = []
 }
 Hunter.prototype = new Sprite
 Hunter.prototype.draw = function() {
   var img = new Image
   img.src = this.src[this.dir]
-  this.stage.context.drawImage(img, this.x, this.y)
+  this.game.context.drawImage(img, this.x, this.y)
 }
 Hunter.prototype.onenterframe = function() {
+  for (var i = 0; i < this.distance; i++) {
+    if (this.path.length > 0) {
+      var p = this.path.shift()
+      this.x = p.x
+      this.y = p.y
+    }
+  }
+  this.draw()
+}
+Hunter.prototype.buildPath = function(destP) {
   var nextX = this.x
   var nextY = this.y
-  var deltaX = this.stage.click.x - this.x
-  var deltaY = this.stage.click.y - this.y
+  var deltaX = destP.x - this.x
+  var deltaY = destP.y - this.y
   var stepX, stepY
   var step = 0
   var fraction = 0
-  var line = []
+  this.path = []
 
   if (deltaX < 0) {
     stepX = -1
@@ -85,51 +107,40 @@ Hunter.prototype.onenterframe = function() {
   deltaX = Math.abs(deltaX * 2)
   deltaY = Math.abs(deltaY * 2)
 
-  line[step] = new Point(nextX, nextY)
+  if (deltaX <= deltaY) {
+    if (this.y < destP.y) this.dir = 's'
+    if (this.y > destP.y) this.dir = 'n'
+  } else {
+    if (this.x < destP.x) this.dir = 'e'
+    if (this.x > destP.x) this.dir = 'w'
+  }
+
+  this.path[step] = new Point(nextX, nextY)
   step++
 
   if (deltaX > deltaY) {
     fraction = deltaY - deltaX / 2;
-    while (nextX != this.stage.click.x) {
+    while (nextX != destP.x) {
       if (fraction >= 0) {
         nextY += stepY
         fraction -= deltaX
       }
       nextX += stepX
       fraction += deltaY
-      line[step] = new Point(nextX, nextY)
+      this.path[step] = new Point(nextX, nextY)
       step++
     }
   } else {
     fraction = deltaX - deltaY / 2
-    while (nextY != this.stage.click.y) {
+    while (nextY != destP.y) {
       if (fraction >= 0) {
         nextX += stepX
         fraction -= deltaY
       }
       nextY += stepY
       fraction += deltaX
-      line[step] = new Point(nextX, nextY)
+      this.path[step] = new Point(nextX, nextY)
       step++
     }
   }
-
-  if (line.length > 1) {
-    this.x = line[1].x
-    this.y = line[1].y
-  }
-
-  this.draw()
-}
-
-function Tile() {
-}
-Tile.prototype = new Sprite
-Tile.prototype.draw = function() {
-  this.stage.context.beginPath()
-  this.stage.context.fillStyle = 'rgb(128, 128, 128)'
-  this.stage.context.fillRect(0, 0, 256, 256)
-}
-Tile.prototype.onenterframe = function() {
-  this.draw()
 }
